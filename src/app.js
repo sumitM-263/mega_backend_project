3166a55b4c37ed9417e2dad00431991d23afcd28
Sumit Maurya <hsumit263@gmail.com>
2025-12-30 16:38:30 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -17,4 +17,15 @@ app.use(express.urlencoded({
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration (!important)
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
+
+
+
+
 export {app};
